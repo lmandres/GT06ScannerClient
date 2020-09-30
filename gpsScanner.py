c7d55411fbaa7c0eed57a781bde229139cd26607
Leo Andres <justme@sanjuan>
2020-09-30 10:37:49 -0400

    Adding inital files for GT06ScannerClient.

---
+++ gpsScanner.py
@@ -0,0 +1,207 @@
+import _thread
+import math
+import re
+import time
+
+import serial
+
+
+class GPSScanner():
+
+    serialPort = None
+    serialBaud = None
+    gpsSerial = None
+
+    locationHash = None
+    runLocationThread = None
+
+    def __init__(self, serialPortIn=None, serialBaudIn=None):
+        if serialPortIn:
+            self.serialPort = serialPortIn
+        if serialBaudIn:
+            self.serialBaud = serialBaudIn
+
+    def resetConnection(self):
+        try:
+            self.gpsSerial.close()
+            self.gpsSerial = serial.Serial(
+                self.serialPort,
+                self.serialBaud
+            )
+        except:
+            pass
+
+    def getSerialBaud(self):
+        return self.serialBaud
+
+    def setSerialBaud(self, serialBaudIn):
+        self.serialBaud = serialBaudIn
+
+    def getSerialPort(self):
+        return self.serialPort
+
+    def setSerialPort(self, serialPortIn):
+        self.serialPort = serialPortIn
+
+    def getLocationHash(self):
+        return self.locationHash
+
+    def runLocate(self):
+
+        def doThreadLoop():
+            self.locationHash = self.doDecodeGPS()
+            while self.runLocationThread:
+                self.locationHash.update(self.doDecodeGPS())
+
+        self.locationHash = {}
+        self.runLocationThread = True
+
+        self.gpsSerial = serial.Serial(self.serialPort, self.serialBaud)
+        _thread.start_new_thread(doThreadLoop, ())
+
+    def stopLocate(self):
+        self.runLocationThread = False
+        self.gpsSerial.close()
+
+    def doDecodeGPS(self):
+
+        def doGPSCheckSum(stringIn):
+            checkSum = 0
+            for charIndex in stringIn:
+                checkSum ^= ord(charIndex)
+            return str(hex(checkSum)).upper()[2:]
+
+        returnHash = {}
+
+        match = None
+        while True:
+
+            serialLine = b""
+            try:
+                serialLine = self.gpsSerial.readline()
+            except:
+                self.resetConnection()
+
+            match = re.match(
+                b'^\$(GPGGA|GPRMC|GPGLL|GPGSV),(.*)\*(.*)',
+                serialLine
+            )
+            if match:
+                 checkStr = "{},{}".format(
+                     match.group(1).decode("utf-8"),
+                     match.group(2).decode("utf-8")
+                 )
+                 funcSum = doGPSCheckSum(checkStr)
+                 gpsSum = str(match.group(3).decode("utf-8"))[:2]
+                 if funcSum == gpsSum:
+                     break
+        if match.group(1) == b'GPRMC':
+            returnHash = self.decodeGPRMC(match.group(2).decode('utf-8'))
+        elif match.group(1) == b'GPGGA':
+            returnHash = self.decodeGPGGA(match.group(2).decode('utf-8'))
+        elif match.group(1) == b'GPGLL':
+            returnHash = self.decodeGPGLL(match.group(2).decode('utf-8'))
+        elif match.group(1) == b'GPGSV':
+            returnHash = self.decodeGPGSV(match.group(2).decode('utf-8'))
+
+        return returnHash
+
+    def decodeGPGLL(self, stringIn):
+
+        returnHash = {}
+        gpsArray = stringIn.split(',')
+
+        try:
+
+            returnHash['latitude'] = ((float(gpsArray[0]) - math.floor(float(gpsArray[0]) / 100) * 100) / 60) + math.floor(float(gpsArray[0]) / 100)
+            returnHash['latitudeDir'] = gpsArray[1]
+
+            returnHash['longitude'] = ((float(gpsArray[2]) - math.floor(float(gpsArray[2]) / 100) * 100) / 60) + math.floor(float(gpsArray[2]) / 100)
+            returnHash['longitudeDir'] = gpsArray[3] 
+
+            returnHash['time'] = str(gpsArray[4][0:2]) + ':' + str(gpsArray[4][2:4]) + ':' + str(gpsArray[4][4:6])
+            returnHash['timestamp'] = gpsArray[4]
+
+        except IndexError:
+            pass
+        except ValuerError:
+            pass
+
+        return returnHash
+
+    def decodeGPGGA(self, stringIn):
+
+        returnHash = {}
+        gpsArray = stringIn.split(',')
+
+        try:
+
+            returnHash['latitude'] = ((float(gpsArray[1]) - math.floor(float(gpsArray[1]) / 100) * 100) / 60) + math.floor(float(gpsArray[1]) / 100)
+            returnHash['latitudeDir'] = gpsArray[2]
+
+            returnHash['longitude'] = ((float(gpsArray[3]) - math.floor(float(gpsArray[3]) / 100) * 100) / 60) + math.floor(float(gpsArray[3]) / 100)
+            returnHash['longitudeDir'] = gpsArray[4]
+
+            returnHash['altitude'] = float(gpsArray[8])
+            returnHash['altitudeUnits'] = gpsArray[9]
+
+            returnHash['time'] = str(gpsArray[0][0:2]) + ':' + str(gpsArray[0][2:4]) + ':' + str(gpsArray[0][4:6])
+            returnHash['timestamp'] = gpsArray[0]
+
+        except IndexError:
+            pass
+        except ValueError:
+            pass
+
+        return returnHash
+
+    def decodeGPRMC(self, stringIn):
+
+        returnHash = {}
+        gpsArray = stringIn.split(',')
+
+        try:
+
+            returnHash['latitude'] = ((float(gpsArray[2]) - math.floor(float(gpsArray[2]) / 100) * 100) / 60) + math.floor(float(gpsArray[2]) / 100)
+            returnHash['latitudeDir'] = gpsArray[3]
+
+            returnHash['longitude'] = ((float(gpsArray[4]) - math.floor(float(gpsArray[4]) / 100) * 100) / 60) + math.floor(float(gpsArray[4]) / 100)
+            returnHash['longitudeDir'] = gpsArray[5]
+
+            returnHash['groundSpeedKnots'] = float(gpsArray[6])
+            returnHash['course'] = float(gpsArray[7])
+
+            returnHash['date'] = str(int(gpsArray[8][4:6])) + '-' + str(gpsArray[8][2:4]) + '-' + str(gpsArray[8][0:2])
+            returnHash['time'] = str(gpsArray[0][0:2]) + ':' + str(gpsArray[0][2:4]) + ':' + str(gpsArray[0][4:6])
+            returnHash['timestamp'] = gpsArray[0]
+
+        except IndexError:
+            pass
+        except ValueError:
+            pass
+
+        return returnHash
+
+    def decodeGPGSV(self, stringIn):
+
+        returnHash = {}
+        gpsArray = stringIn.split(',')
+
+        try:
+            returnHash["satellitesInView"] = gpsArray[2]
+        except IndexError:
+            pass
+
+        return returnHash
+
+if __name__ == '__main__':
+
+    gpsScanner = GPSScanner("/dev/ttyUSB_GPS0", 4800)
+    gpsScanner.runLocate()
+    while True:
+        try:
+            print(gpsScanner.getLocationHash())
+            time.sleep(1)
+        except KeyboardInterrupt as ki:
+            break
+    gpsScanner.stopLocate()
---
+++ gt06Client.py
@@ -0,0 +1,160 @@
+import codecs
+import socket
+
+import crc_itu
+
+
+class GT06Client():
+
+    serverAddress = None
+    serverPort = None
+    timeOut = None
+
+    infoSerial = 0
+    socket = None
+
+    def __init__(self, serverAddress=None, serverPort=None, timeOut=60):
+        if serverAddress:
+            self.serverAddress = serverAddress
+        if serverPort:
+            self.serverPort = serverPort
+        self.timeOut = timeOut
+
+    def getServerPort(self):
+        return self.serverPort
+
+    def setServerPort(self, serverPort):
+        self.serverPort = serverPort
+
+    def getServerAddress(self):
+        return self.serverAddress
+
+    def setServerAddress(self, serverAddress):
+        self.serverAddress = serverAddress
+
+    def getTimeOut(self):
+        return self.timeOut
+
+    def setTimeOut(self, timeOut):
+        self.timeOut = timeOut
+
+    def connect(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((self.serverAddress, self.serverPort))
+        self.socket.settimeout(self.timeOut)
+
+    def disconnect(self):
+        self.socket.close()
+
+    def makeBasicMessage(self, protocolIn, messageIn): 
+
+        self.infoSerial += 1
+        self.infoSerial %= 0x10000
+
+        returnBytes = None
+
+        packetLen = 0
+        try:
+            packetLen = int(len(bytes.fromhex(messageIn))+5)
+        except ValueError:
+            pass
+
+        crcCheckFormat = "{:02x}{:02x}{}{:04x}"
+        crcCheckString = crcCheckFormat.format(
+            packetLen,
+            int(protocolIn),
+            messageIn,
+            int(self.infoSerial)
+        )
+
+        messageFormat = "7878{}{:04x}0d0a"
+
+        errorCheck = 0
+        try:
+            errorCheck = crc_itu.crc16(bytes.fromhex(crcCheckString))
+        except ValueError:
+            pass
+
+        messageOut = messageFormat.format(
+            crcCheckString,
+            errorCheck
+        )
+
+        returnBytes = bytes.fromhex(messageOut)
+
+        return returnBytes
+
+    def makeBasicMessageResponse(self, protocolIn):
+
+        returnBytes = None
+
+        crcCheckFormat = "{:02x}{:02x}{:04x}"
+        crcCheckString = crcCheckFormat.format(
+            5,
+            int(protocolIn),
+            int(self.infoSerial)
+        )
+            
+        messageFormat = "7878{}{:04x}0d0a"
+
+        errorCheck = 0
+        try:
+            errorCheck = crc_itu.crc16(bytes.fromhex(crcCheckString))
+        except ValueError:
+            pass
+
+        messageOut = messageFormat.format(
+            crcCheckString,
+            errorCheck
+        )
+
+        returnBytes = bytes.fromhex(messageOut)
+
+        return returnBytes
+
+    def sendLoginMessage(self, imeiIn):
+
+        serverResp = None
+
+        try:
+            int(imeiIn)
+        except ValueError:
+            raise ValueError("IMEI string contains non-numeric digits.")
+
+        self.socket.sendall(self.makeBasicMessage(1, imeiIn))
+        try:
+            serverResp = self.socket.recv(1024)
+        except socket.timeout:
+            pass
+
+        if self.makeBasicMessageResponse(1) != serverResp:
+            return False
+        else:
+            return True
+
+    def sendGPSMessage(self, gpsHash):
+
+        serverResp = None
+
+        try:
+            bytes.fromhex(gpsHash)
+        except ValueError:
+            raise ValueError("GPS hash string contains non-hexadecimal digits.")
+
+        self.socket.sendall(self.makeBasicMessage(16, gpsHash))
+        try:
+            serverResp = self.socket.recv(1024)
+        except socket.timeout:
+            pass
+
+        if self.makeBasicMessageResponse(1) != serverResp:
+            return False
+        else:
+            return True
+
+if __name__ == "__main__":
+    gt06client = GT06Client("10.0.0.245", 5023)
+    gt06client.connect()
+    gt06client.sendLoginMessage("0123456789012345")
+    gt06client.sendGPSMessage("0b081d112e10cf027ac7eb0c46584900148f")
+    gt06client.disconnect()
